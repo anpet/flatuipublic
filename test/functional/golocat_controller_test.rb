6b35bc3e07e19e81cf6e76aeda8bb44474c2f287
anpet <anpet@naver.com>
2013-08-01 11:56:36 +0900

    앱폴더 추가

---
+++ test/functional/golocat_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GolocatControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/helpers/golocat_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class GolocatHelperTest < ActionView::TestCase
+end
